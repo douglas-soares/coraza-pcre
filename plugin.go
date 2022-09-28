1d2e56df226042816d6e3f5e082baa00e4e89953
douglas-soares <douglas.soares@incognia.com>
2022-09-28 17:48:34 -0300

    feat: remove plugin logic to test performance

--- plugin.go
+++ plugin.go
@@ -15,9 +15,6 @@
 package plugin
 
 import (
-	"regexp"
-	"strings"
-
 	"github.com/corazawaf/coraza/v3"
 	"github.com/corazawaf/coraza/v3/operators"
 	"github.com/gijsbers/go-pcre"
@@ -31,47 +28,10 @@ type rx struct {
 }
 
 func (o *rx) Init(options coraza.RuleOperatorOptions) error {
-	data := options.Arguments
-	var err error
-	re, err := regexp.Compile(`%{.*}`)
-	if err != nil {
-		return err
-	}
-	macros := re.FindAllString(data, -1)
-	if len(macros) > 0 {
-		o.macro, err = coraza.NewMacro(macros[0])
-		if err != nil {
-			return err
-		}
-	} else {
-		o.compiled = true
-		o.re, err = pcre.Compile(data, pcre.DOTALL|pcre.DOLLAR_ENDONLY)
-	}
-	o.data = data
-	return err
+	return nil
 }
 func (o *rx) Evaluate(tx *coraza.Transaction, value string) bool {
-	if !o.compiled && o.macro != nil {
-		var err error
-		o.re, err = pcre.Compile(strings.Replace(o.data, o.macro.String(), o.macro.Expand(tx), -1), pcre.DOTALL|pcre.DOLLAR_ENDONLY)
-		if err != nil {
-			tx.WAF.Logger.Error("@rx operator compile macro data error", err)
-			return false
-		}
-		o.compiled = true
-
-	}
-
-	m := o.re.MatcherString(value, 0)
-	if tx.Capture {
-		for i := 0; i < m.Groups()+1; i++ {
-			if i == 10 {
-				return true
-			}
-			tx.CaptureField(i, m.GroupString(i))
-		}
-	}
-	return m.Matches()
+	return false
 }
 
 func init() {
